c93783d8f92d802e385d07c7126569e0c6384560
adrienw4 <adrienweykmans5@gmail.com>
2023-08-03 12:03:29 +0200

    new up 7

--- scripts.js
+++ scripts.js
@@ -1,61 +1,69 @@
 //exo 1
-let myAlphabet = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-    "$",
-    "*",
-    "/",
-    "-",
-    "+",
-  ];
- 
-function myArrayLength(myArray) {
-    console.log(myArray.length);
-    
-}  
-myArrayLength(myAlphabet);
+// let myAlphabet = [
+//     "A",
+//     "B",
+//     "C",
+//     "D",
+//     "E",
+//     "F",
+//     "G",
+//     "H",
+//     "I",
+//     "J",
+//     "K",
+//     "L",
+//     "M",
+//     "N",
+//     "O",
+//     "P",
+//     "Q",
+//     "R",
+//     "S",
+//     "T",
+//     "U",
+//     "V",
+//     "W",
+//     "X",
+//     "Y",
+//     "Z",
+//     "$",
+//     "*",
+//     "/",
+//     "-",
+//     "+",
+//   ];
+// const planet = ["mars","neptune","terre","venus","jupiter"]
+// console.log(myAlphabet.length);
+  
+//   function myArrayLength(x) {
+//       console.log(x.length);
+// }  
+// myArrayLength(myAlphabet);
+// myArrayLength(planet);
+
 // length 31
 
 //exo 2
-const planets = ["mars","neptune","terre","venus","jupiter"]
-planets.forEach((planet,index) => {
-    console.log(planet, index);
-    });
+// const planets = ["mars","neptune","terre","venus","jupiter"]
+// planets.forEach((planet,index) => {
+//     console.log(planet, index);
+//     });
 // exo 3
-const someDataTypes = ["5",12,`5`, null]
+// const someDataTypes = ["adrien",12,true, null]
+// someDataTypes.forEach((data, index) => {
+//   console.log(typeof data, index);
+// });
+
+//exo 4
+const newArrray = [];
+console.log(newArrray);
+const copyNewarray = newArrray.slice();
+console.log(copyNewarray);
+copyNewarray.push("Kiwi","mandarine",1,45);
 
 
-
-
-
-
-
-
-
+// exo 5
+const furnitures = ["table","chairs","couch"]
 
 
 
@@ -68,8 +76,10 @@ const someDataTypes = ["5",12,`5`, null]
 
 
 // exo 6
-let values1= ["Apple", 1, false, "2"];
-let values2 = [`5`, "Fries", 2 , true];
-let values3 = ["Mars", "Strawberry", 9];
-
-
+// let values1= ["Apple", 1, false, "2"];
+// let values2 = [`5`, "Fries", 2 , true];
+// let values3 = ["Mars", "Strawberry", 9];
+// const valuesArray = values1.concat(values2,values3)
+// console.log(valuesArray);
+// const arrNumbers =valuesArray .filter((val) => typeof val === "number");
+// console.log(arrNumbers);
